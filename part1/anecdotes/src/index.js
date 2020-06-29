2e6082e6be5bbc108193eee47cdced6c2d81293e
RyanSamman <RyanSamman@outlook.com>
2020-06-29 18:12:56 +0300

    Anecdotes component

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,18 +1,53 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
+const Votes = ({votes}) => {
+if (votes) return <p>has {votes} votes</p>
+  return <></>
+}
+
+const Anecdote = ({anecdote, votes}) => {
+  return (
+    <>
+      <div>{anecdote}</div>
+      <Votes votes={votes}/>
+    </>
+  )
+}
+
+const GreatestVotes = ({anecdotes, anecdoteArray, greatest}) => {
+  console.log(anecdotes, anecdoteArray, greatest)
+  if (!greatest) return <></>
+  return <Anecdote anecdote={anecdotes[greatest]} votes={anecdoteArray[greatest]} />
+}
+
+const App = ({anecdotes}) => {
   const [selected, setSelected] = useState(() => Math.floor(Math.random() * anecdotes.length))
   const selectRandomAnecdote = () => setSelected(Math.floor(Math.random() * anecdotes.length))
 
+  const [anecdoteArray, setAnecdoteArray] = useState(() => Array(6).fill(0))
 
+  const [greatest, setGreatest] = useState()
+
+  const addAnecdoteVote = () => {
+    const newArray = [...anecdoteArray]
+    newArray[selected]++
+    setAnecdoteArray(newArray)
+
+    let x = anecdoteArray.reduce((grtest, currentValue, currentIndex, array) => {
+      console.log(array[currentIndex], array[grtest] , currentIndex, )
+      if (array[currentIndex] > array[grtest]) return currentIndex
+      return grtest
+    }, 0)
+    console.log({x})
+  }
 
   return (
     <>
-      <div>
-        {props.anecdotes[selected]}
-      </div>
+      <Anecdote greatest={greatest} anecdote={anecdotes[selected]} votes={anecdoteArray[selected]} />
+      <button onClick={addAnecdoteVote}>vote</button>
       <button onClick={selectRandomAnecdote}>next anectote</button>
+      <GreatestVotes greatest={greatest} anecdotes={anecdotes} anecdoteArray={anecdoteArray}/>
     </>
   )
 }
