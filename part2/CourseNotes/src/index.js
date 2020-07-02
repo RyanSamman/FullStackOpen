921f8f661253b3fad23f04d1041d172ac98bc8b0
RyanSamman <RyanSamman@outlook.com>
2020-07-03 02:23:42 +0300

    Finished course Part 2.e - React & CSS

--- part2/CourseNotes/src/index.js
+++ part2/CourseNotes/src/index.js
@@ -1,23 +1,62 @@
 import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
-import axios from 'axios'
+import noteService from './services/notes'
 
-const Note = ({ note }) => <li>{note.content}</li>
+import './index.css'
+
+const Footer = () => {
+	const footerStyle = {
+		color: 'green',
+		fontStyle: 'italic',
+		fontSize: 16
+	}
+
+	return (
+		<div style={footerStyle}>
+			<br />
+			<em>Note app, Department of Computer Science, University of Helsinki 2020</em>
+		</div>
+	)
+}
+
+const Notification = ({ message }) => {
+	if (message === null) {
+		return null
+	}
+
+	return (
+		<div className="error">
+			{message}
+		</div>
+	)
+}
+
+const Note = ({ note, toggleImp }) => {
+	return (
+		<li>
+			{note.content}
+			<button onClick={toggleImp}>
+				Make {note.important ? 'Not Important' : 'Important'}
+			</button>
+		</li>
+
+	)
+}
 
 const App = () => {
 	const [notes, setNotes] = useState([])
 	const [newNote, setNewNote] = useState('')
 	const [showAll, setShowAll] = useState(true)
+	const [errorMessage, setErrorMessage] = useState(null)
 
 	useEffect(() => {
-		axios
-			.get('http://localhost:3001/notes')
-			.then((response) => setNotes(response.data))
+		noteService.getAll()
+			.then(initialNotes => setNotes(initialNotes))
 	}, [])
 
 	// Need to update the server when a new note is added...
 
-	const addNote = (event) => {
+	const addNote = event => {
 		event.preventDefault()
 
 		const noteObject = {
@@ -27,8 +66,28 @@ const App = () => {
 			id: notes.length + 1
 		}
 
-		setNotes(notes.concat(noteObject))
-		setNewNote('')
+		noteService.create(noteObject)
+			.then(returnedNote => {
+				setNotes(notes.concat(returnedNote))
+				setNewNote('')
+			})
+	}
+
+	const toggleImp = id => {
+		const toggledNote = notes.find(note => note.id === id)
+		const changedNote = { ...toggledNote, important: !toggledNote.important }
+		noteService.update(id, changedNote)
+			.then(returnedNote => {
+				setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+			})
+			.catch(error => {
+				setErrorMessage(`Note '${changedNote.content}' was already deleted from the server`)
+
+				setTimeout(() => {
+					setErrorMessage(null)
+				}, 5000)
+				setNotes(notes.filter(note => note.id !== id))
+			})
 	}
 
 	const shownNotes = showAll
@@ -38,14 +97,17 @@ const App = () => {
 	return (
 		<div>
 			<h1>Notes</h1>
+			<Notification message={errorMessage} />
 			<ul>
-				{shownNotes.map(note => <Note key={note.id} note={note} />)}
+				{shownNotes.map(note =>
+					<Note key={note.id} note={note} toggleImp={() => toggleImp(note.id)} />)}
 			</ul>
 			<form onSubmit={addNote}>
 				<input value={newNote} onChange={(event) => setNewNote(event.target.value)}></input>
 				<button type="submit">save</button>
 			</form>
 			<button onClick={() => setShowAll(!showAll)}>{showAll ? "Show Important" : "Show All"}</button>
+			<Footer />
 		</div>
 	)
 }
