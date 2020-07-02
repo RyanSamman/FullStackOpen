8cbfbe9310cbbe29296db4462f3185f6b2e54ced
RyanSamman <RyanSamman@outlook.com>
2020-07-02 21:23:25 +0300

    Finished problems 2.15 - 2.18 - Phonebook CRUD
    json-server CRUD operations to store people's names & numbers

--- part2/phonebook/src/components/People.js
+++ part2/phonebook/src/components/People.js
@@ -1,8 +1,29 @@
 import React from 'react'
 
-const People = ({ people }) => (
-	<ul>
-		{people.map((person) => <li key={person.name}>{person.name} {person.number}</li>)}
-	</ul>)
+import phonebookService from '../services/phonebookService'
+
+const People = ({ people, setPeople }) => {
+
+	const deletePerson = personToDelete => {
+		if (window.confirm(`Delete ${personToDelete.name} ?`)) {
+			phonebookService.deletePerson(personToDelete.id)
+				.then((deletedPerson) => {
+					setPeople(people.filter(person => person.id !== personToDelete.id))
+				})
+		}
+	}
+
+	return (
+		<ul>
+			{people.map(person => {
+				return <li key={person.name}>
+					{person.name} {person.number}
+					<button onClick={() => deletePerson(person)}>Delete</button>
+				</li>
+			})}
+		</ul >
+	)
+}
+
 
 export default People
--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -1,14 +1,36 @@
 import React, { useState } from 'react'
 
+// Services
+import phonebookService from '../services/phonebookService'
+
 const PersonForm = ({ people, setPeople }) => {
 	const [formName, setFormName] = useState('')
 	const [formNumber, setFormNumber] = useState('')
 
 	const addPerson = (event) => {
 		event.preventDefault()
-		if (people.find((person) => person.name === formName)) return alert(`${formName} is already added to the phonebook`)
-		setPeople(people.concat({ name: formName, number: formNumber }))
-		setFormName('')
+		let newPerson = { name: formName, number: formNumber }
+		const match = people.find((person) => person.name === formName)
+		if (match) {
+			const isConfirmed = window.confirm(`${match.name} is already added to the phonebook, replace the old number with a new one?`)
+			if (!isConfirmed) return
+			return phonebookService.updatePerson(match.id, newPerson)
+				.then(updatedPerson => {
+					setPeople(people.map(person => person.id === updatedPerson.id
+						? updatedPerson
+						: person
+					))
+					setFormName('')
+					setFormNumber('')
+				})
+		}
+
+		phonebookService.createPerson(newPerson)
+			.then(createdPerson => {
+				setPeople(people.concat(createdPerson))
+				setFormName('')
+				setFormNumber('')
+			})
 	}
 
 	return (
