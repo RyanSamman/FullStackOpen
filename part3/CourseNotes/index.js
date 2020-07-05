889c024bd2d8c6a878a4688f86eb4929bcb0f50e
RyanSamman <RyanSamman@outlook.com>
2020-07-06 01:08:14 +0300

    3a - Logger

--- part3/CourseNotes/index.js
+++ part3/CourseNotes/index.js
@@ -1,36 +1,45 @@
 const express = require('express')
-const { request } = require('express')
 const app = express()
+
+const requestLogger = (req, res, next) => {
+	console.log('Method:', req.method)
+	console.log('Path:', req.path)
+	console.log('Body:', req.body)
+	console.log('-------')
+	next()
+}
+
+app.use(requestLogger);
 
 app.use(express.json())
 
 let notes = [
 	{
-	  id: 1,
-	  content: "HTML is easy",
-	  date: "2019-05-30T17:30:31.098Z",
-	  important: true
+		id: 1,
+		content: "HTML is easy",
+		date: "2019-05-30T17:30:31.098Z",
+		important: true
 	},
 	{
-	  id: 2,
-	  content: "Browser can execute only Javascript",
-	  date: "2019-05-30T18:39:34.091Z",
-	  important: false
+		id: 2,
+		content: "Browser can execute only Javascript",
+		date: "2019-05-30T18:39:34.091Z",
+		important: false
 	},
 	{
-	  id: 3,
-	  content: "GET and POST are the most important methods of HTTP protocol",
-	  date: "2019-05-30T19:20:14.298Z",
-	  important: true
+		id: 3,
+		content: "GET and POST are the most important methods of HTTP protocol",
+		date: "2019-05-30T19:20:14.298Z",
+		important: true
 	}
 ]
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+	res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+	res.json(notes)
 })
 
 app.get('/api/notes/:id', (request, response) => {
@@ -42,29 +51,29 @@ app.get('/api/notes/:id', (request, response) => {
 
 const generateId = () => {
 	const maxId = notes.length > 0
-	  	? Math.max(...notes.map(n => n.id))
-	  	: 0
+		? Math.max(...notes.map(n => n.id))
+		: 0
 	return maxId + 1
-  }
-  
+}
+
 app.post('/api/notes', (request, response) => {
 	const body = request.body
-  
+
 	if (!body.content) {
-	  	return response.status(400).json({ 
-			error: 'content missing' 
-	  })
+		return response.status(400).json({
+			error: 'content missing'
+		})
 	}
-  
+
 	const note = {
-	  	content: body.content,
-	  	important: body.important || false,
-	  	date: new Date(),
-	  	id: generateId(),
+		content: body.content,
+		important: body.important || false,
+		date: new Date(),
+		id: generateId(),
 	}
-  
+
 	notes = notes.concat(note)
-  
+
 	response.json(note)
 })
 
@@ -75,7 +84,9 @@ app.delete('/api/notes/:id', (req, res) => {
 	res.status(204).end()
 })
 
+app.use((_req, res) => res.status(404).json({ error: 'unknown endpoint' }))
+
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+	console.log(`Server running on port ${PORT}`)
 })
